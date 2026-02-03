d1388e3d643daa63bab7576f1b750eb88b5877de
jzzmiiinn <jzzmiiinnali@gmail.com>
2026-02-03 18:37:38 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1>Hello SPEC Developers </h1>
+</template>
